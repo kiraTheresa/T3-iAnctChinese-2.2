afe2c4d8446a7aad8c1e160bdbd14441f0d38780
kiraTheresa <2312190107@pop.zjgsu.edu.cn>
2025-12-05 20:19:06 +0800

    金沙湖

---
+++ backend/BiograFi-Backend/src/main/java/com/zjgsu/kirateresa/BiograFi_Backend/BiograFiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zjgsu.kirateresa.BiograFi_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiograFiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiograFiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/BiograFi-Backend/src/test/java/com/zjgsu/kirateresa/BiograFi_Backend/BiograFiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjgsu.kirateresa.BiograFi_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiograFiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
