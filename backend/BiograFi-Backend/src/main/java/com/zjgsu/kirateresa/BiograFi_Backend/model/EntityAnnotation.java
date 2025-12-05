81372aca0504131ad01fa782f3f2ad437fb0758f
kiraTheresa <2312190107@pop.zjgsu.edu.cn>
2025-12-05 21:08:44 +0800

    客运中心（可换乘九号线）

--- backend/BiograFi-Backend/src/main/java/com/zjgsu/kirateresa/BiograFi_Backend/model/EntityAnnotation.java
+++ backend/BiograFi-Backend/src/main/java/com/zjgsu/kirateresa/BiograFi_Backend/model/EntityAnnotation.java
@@ -6,9 +6,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 实体标注模型
--- backend/BiograFi-Backend/src/main/java/com/zjgsu/kirateresa/BiograFi_Backend/model/Project.java
+++ backend/BiograFi-Backend/src/main/java/com/zjgsu/kirateresa/BiograFi_Backend/model/Project.java
@@ -1,13 +1,12 @@
 package com.zjgsu.kirateresa.BiograFi_Backend.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
